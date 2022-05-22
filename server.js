705d6d0ae170feca808ccebe7c4d90c57294ec04
Koehler <ckoehler16@gmail.com>
2022-05-22 19:52:47 -0400

    created database connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,9 +8,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // my MySQL username,
+        user: 'root',
+        // my MySQL password
+        password: 'lola',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
-// Default response for any other request (Not Found) ** place above 'Listen'
+// Catchall route; Default response for any other request (Not Found) ** place above 'Listen'
 app.use((req, res) => {
     res.status(404).end();
 });
